edb624369d9a93432598c19c779685e0c5a1acf5
Dior.Shelton <dior.shelton@gmail.com>
2021-11-02 10:50:31 -0400

    Add placeholder to text area tags

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
-// const main = document.querySelector(".main");
-// const input = document.createElement("input");
-//     input.setAttribute("type", "text");
-//     main.appendChild(input);
+const main = document.querySelector(".main");
+const pinned = document.querySelector(".pinned");
 
+// create elements
+let title
 // fetch ("http://localhost:3000/notes")
 // .then(response => response.json())
 // .then(function(notesArray) {
