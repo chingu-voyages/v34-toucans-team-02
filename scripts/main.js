d27592fed0c34f6c93244891c9ae6bdb3cf8f8c8
Dior.Shelton <dior.shelton@gmail.com>
2021-11-02 08:56:57 -0400

    Add text area tag and css styling to note

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
-fetch ("http://localhost:3000/notes")
-.then(response => response.json())
-.then(function(notesArray) {
-    console.log(notesArray)
-})
+// const main = document.querySelector(".main");
+// const input = document.createElement("input");
+//     input.setAttribute("type", "text");
+//     main.appendChild(input);
+
+// fetch ("http://localhost:3000/notes")
+// .then(response => response.json())
+// .then(function(notesArray) {
+//     console.log(notesArray)
+// })
 
 //json-server --watch db.json --port 3000
